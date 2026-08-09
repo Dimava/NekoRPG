d145b9f9d8edf271d9f9c3fdc13669c6c44f3d31
btlye308 <120305480+btly0711@users.noreply.github.com>
2026-08-10 00:12:52 +0800

    V3.31f
    i hope there is no more

--- src/main.js
+++ src/main.js
@@ -4611,9 +4611,9 @@ function load(save_data) {
     //otherwise just do simple substraction
     //then same with s.t.a.m.i.n.a below
 
-    //WIP:天道封锁【V3.31e-V3.41】，将在V3.41删除
-    if(character.stats.flat.gems.attack_power >= 1616.57e8 || character.stats.flat.gems.attack_power.defense >= 1616.57e8 || character.stats.flat.gems.agility >= 1616.57e8 || character.stats.flat.gems.max_health >= 32.3315e12){
-        character.stats.flat.gems.attack_power = character.stats.flat.gems.attack_power.defense = character.stats.flat.gems.agility = character.stats.flat.gems.max_health = 0;
+    //WIP:大坍缩【V3.31e-V3.41】，将在V3.41删除
+    if(character.stats.flat.gems.attack_power >= 1616.57e8 || character.stats.flat.gems.defense >= 1616.57e8 || character.stats.flat.gems.agility >= 1616.57e8 || character.stats.flat.gems.max_health >= 32.3315e12){
+        character.stats.flat.gems.attack_power = character.stats.flat.gems.defense = character.stats.flat.gems.agility = character.stats.flat.gems.max_health = 0;
         
         log_message("[纱雪]宝石的力量超越了极限……触发了【大坍缩】！","sayuki");
         log_message("你已获取1无限点，请移步AntiNeko Dimensions 领取。","sayuki");
